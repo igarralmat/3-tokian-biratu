f8a125b2f09eaeacf92f71bcad6f29bd0f0b397f
igarralmat <93262344+igarralmat@users.noreply.github.com>
2022-03-18 14:25:31 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        `)
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
-    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 50)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . # . .
         . . . # .
         # # # # #
         . . . # .
@@ -19,5 +8,19 @@ input.onButtonPressed(Button.B, function () {
         `)
     DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
     DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 50)
+    basic.pause(3000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 50)
+    basic.pause(3000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    basic.clearScreen()
 })
 DFRobotMaqueenPlus.I2CInit()
